83f562a5d4ca5f93c96b38d9d464d265f4293c55
Kristian Ulvund <>
2020-06-18 00:07:15 +0200

    Updates

---
+++ precache-manifest.38b0fb8bd0b0fea6ceef393f62906dae.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8742e5a4e1093b1726d1e45e64c853ff",
+    "url": "/naf/index.html"
+  },
+  {
+    "revision": "9ca0e28db3b2ca6df095",
+    "url": "/naf/static/css/main.c0fa747d.chunk.css"
+  },
+  {
+    "revision": "1bbe09d7eda38fbc77c5",
+    "url": "/naf/static/js/2.5a1ec3d9.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/naf/static/js/2.5a1ec3d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ca0e28db3b2ca6df095",
+    "url": "/naf/static/js/main.e52a5def.chunk.js"
+  },
+  {
+    "revision": "0e5b322e6218971e8dd9",
+    "url": "/naf/static/js/runtime-main.66e6fb18.js"
+  },
+  {
+    "revision": "560a817981dff6503a02a37714b4e2e0",
+    "url": "/naf/static/media/logo.560a8179.svg"
+  }
+]);
